9725c4a64d3bd212ac7938e389c1c05473b138b1
Abdullah Naim <aanaim96@gmail.com>
2023-07-31 20:57:17 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const multer = require('multer');
 const app = express();
 const port = 3000;
 const path = require('path');
@@ -9,6 +10,11 @@ app.use(express.static('public'))
 app.use(express.static('scripts'))
 // app.use(express.bodyParser());
 app.use('/', express.static(path.join(__dirname, '/index.html')))
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 async function grab(url = '') {
     fetch(url).then(data => {
@@ -19,9 +25,28 @@ async function grab(url = '') {
         })
 }
 
-app.post('/sending', function (req, res) {
-    response = res.render('the_template', { name: req.body.name })
+async function grab() {
+    let response = await fetch('http://localhost:3000/sending', {
+        method: 'POST',
+        headers: {
+            "Content-Type": 'application/json'
+        },
+        body: JSON.stringify(response)
+    })
     console.log(response)
+    return response;
+}
+
+app.post('/', function (req, res) {
+    // response = res.render('the_template', { name: req.body.name })
+
+    // grab();
+
+    const stuff = req.body.stuff;
+    console.log(stuff);
+    //res.send(stuff)
+
+
 })
 
 app.listen(port, () => {
